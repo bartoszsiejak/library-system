2d154d36e6a535f448d78f0b9401286e56bc0fcc
Bartosz Siejak <bartosz.siejak@gmail.com>
2025-04-02 20:20:39 +0200

    Add Book class

---
+++ LibrarySystem/Data/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibrarySystem.Data;
+
+public class Book
+{
+    public string Tittle { get;}
+    public string Author { get;}
+    public string Isbn { get;}
+    // public Customer? Borrower { get; private set; } = null;
+}
