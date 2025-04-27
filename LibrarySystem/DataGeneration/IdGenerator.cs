7d1345883490379c2a2259f78c871b5c31106c02
Bartosz Siejak <bartosz.siejak@gmail.com>
2025-04-27 16:09:00 +0200

    Add IdGenerator covered by unit tests

---
+++ LibrarySystem/DataGeneration/IdGenerator.cs
@@ -0,0 +1,16 @@
+﻿using LibrarySystem.Menus.Options.CustomerOption;
+
+namespace LibrarySystem.DataGeneration;
+
+public class IdGenerator(uint id) : IIdGenerator
+{
+    private uint _id = id;
+
+    public uint GetId()
+    {
+        checked
+        {
+            return _id++;
+        }
+    }
+}
